16f9d353f588d6bdf86b6c157b80625d73bf33bf
Jakob <kirbyj3684@tamu.edu>
2024-03-22 12:51:25 -0500

    Added Deactivate, Reactivate, and delete with rspec tests

---
+++ db/migrate/20240321221105_create_deactivated_trainees.rb
@@ -0,0 +1,13 @@
+class CreateDeactivatedTrainees < ActiveRecord::Migration[7.0]
+  def change
+    create_table :deactivated_trainees do |t|
+      t.string :full_name
+      t.integer :height_feet
+      t.integer :height_inches
+      t.float :weight
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/deactivated_trainee_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class DeactivatedTraineeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
