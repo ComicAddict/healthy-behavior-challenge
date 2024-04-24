08865671b07d3ec23e80f4c0bef3f08d1c2fa8de
MACKENZIE_HAMLETT <mackenzie.s.hamlett@gmail.com>
2024-04-23 22:52:05 -0500

    Finish integrating discussion forum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   include SimpleDiscussion::ForumUser
-
   def name
-    "#{first_name} #{last_name}"
+    "#{email}"
   end
-  has_secure_password
-
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :full_name, presence: true
-  has_many :instructor_referrals
-  validates_uniqueness_of :email
 end
