3cfdc745d03394c701b00fe44ae31ad851206233
Jakob Kirby <95430949+thatguy988@users.noreply.github.com>
2024-05-03 15:55:03 -0500

    Update application_helper.rb fixed three failing cucumber tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,6 @@ module ApplicationHelper
   end
 
   def current_user
-    @current_user = User.find(session[:user_id])
+    @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
   end
 end
